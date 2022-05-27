54128034c63fbad3026db3d24202861262079938
20509 <20509@1k129s13.dune.edu>
2022-05-27 16:40:39 +0200

    unit2

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -11,11 +11,13 @@ package javaapplication2;
  */
 public class JavaApplication2 {
 
+    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       int a=10;
     }
     
 }
